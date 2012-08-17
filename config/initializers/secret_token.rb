6f5d101ebaaba4bb38f3546af68dd10f4c2f9a38
jovaroc <jovaroc@gmail.com>
2012-08-17 15:18:05 +0100

    inicio app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colores::Application.config.secret_token = '433f99e0ac0e5308632e22ba54b6a6e3c2a1bfa9a578488427cd70ff3073960efc7835fc9bdc75f9fdfdcd40ae9e660a48dd1d5f67e7e246ab545e49ea346213'
