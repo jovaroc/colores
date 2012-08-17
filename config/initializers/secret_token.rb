00f4c4777b585e5cb62a77660fa6fc1562f510cd
jovaroc <jovaroc@gmail.com>
2012-08-17 20:32:06 +0100

    nueva version

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Colores::Application.config.secret_token = '433f99e0ac0e5308632e22ba54b6a6e3c2a1bfa9a578488427cd70ff3073960efc7835fc9bdc75f9fdfdcd40ae9e660a48dd1d5f67e7e246ab545e49ea346213'
+Colores::Application.config.secret_token = '9889f76dd130e4ccbcac199d6912ec84a22765705ab06cecfaa678ed7f22eb9af6676263c438159353e82323f62ecb4257d0cf49129446a4967751714388edd8'
